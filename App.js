b510ff17d6b82eed53cf9fbc3cdcf7faad5df101
kiran <kiran@neostack.com>
2019-05-05 22:52:30 +0530

    positioning hours hand

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+
+class AnalogClock extends React.Component {
+  render() {
+    return (
+      <View>
+        <ImageBackground
+          source={require("./assets/clock.png")}
+          style={styles.clock}
+        >
+          <Image
+            source={require("./assets/hour-hand.png")}
+            style={styles.hourHand}
+          />
+        </ImageBackground>
+      </View>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Analog clock</Text>
+        <AnalogClock />
       </View>
     );
   }
@@ -14,8 +33,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  clock: {
+    position: "relative",
+    width: 300,
+    height: 300
+  },
+  hourHand: {
+    position: "absolute",
+    top: 90,
+    left: 146
+  }
 });
